b2bc2eb3c5318d20dc0312934618a9ea995f397c
stlbucket <stlbucket@gmail.com>
2018-07-15 17:13:27 -0700

    config

--- db/config/example.js
+++ db/config/example.js
@@ -1,2 +1,1 @@
-process.env.DATABASE_URL = 'postgres://USER:PASSWORD@localhost/fbkt'
-process.env.SCHEMAS_TO_GRAPHQL = 'fbkt_core'
+process.env.DATABASE_URL = 'postgres://USER:PASSWORD@localhost/corz'
